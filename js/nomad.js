d9a2ce482d5a06486a1f7c247b534c58837537dc
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-05-29 15:48:51 +0900

    색상 선택 추가

--- js/nomad.js
+++ js/nomad.js
@@ -1,3 +1,4 @@
+const color = document.getElementById('color');
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -27,6 +28,11 @@ function onLineWidthChange(event) {
     ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
 
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
@@ -34,57 +40,7 @@ canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
 
 lineWidth.addEventListener('change', onLineWidthChange);
+color.addEventListener('change', onColorChange);
 
 
 
-///**********************************/
-
-//1. 집 그리기
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.strokeRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
-
-///**********************************/
-
-//2. 사람 만들기 원!!
-// ctx.fillRect(200 - 35, 200 - 30, 15, 100);
-// ctx.fillRect(350 - 35, 200 - 30, 15, 100);
-// ctx.fillRect(250 - 35, 200 - 30, 70, 200);
-
-// ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-// ctx.fill();
-
-// ctx.beginPath();
-// ctx.fillStyle = 'white';
-// ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-// ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-// ctx.fill();
-
-///**********************************/
-
-//3. 마우스 따라서 선 생성 (색 랜덤으로 바뀜)
-// function onClick(event) {
-//     ctx.beginPath();
-//     ctx.moveTo(0, 0);
-//     const color = colors[Math.floor(Math.random() * colors.length)];
-//     ctx.strokeStyle = color;
-//     ctx.lineTo(event.offsetX, event.offsetY);
-//     ctx.stroke();
-// }
-
-// const colors = [
-//     '#C3FF93',
-//     '#FFDB5C',
-//     '#FFAF61',
-//     '#FF70AB',
-//     '#CAF4FF',
-//     '#5AB2FF'
-// ];
-
-// canvas.addEventListener('mousemove', onClick);
